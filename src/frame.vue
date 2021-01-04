3b11b9af6d7b21ffddf4b36f638e639fab3b0838
hansonyu <hansonyu@bccbbs.com>
2021-01-04 16:21:22 +0800

    debug

--- src/frame.vue
+++ src/frame.vue
@@ -4,7 +4,7 @@
             <div class="welcome-header">數位名片<br>{{line_username}} ，您好</div>
             <div class="welcome-body">
             <div v-if="hasCard" class="h100"><button class="h100" @click="sendCard(myCard)">發送自己名片</button></div><br>
-            <!-- <div>{{debug}}<button @click="close">x</button></div> -->
+            <div>{{debug}}<button @click="close">x</button></div>
             </div>
         </div>
         <div>發送同事名片:</div>
@@ -38,8 +38,12 @@ module.exports = {
     },
     mounted(){
         let urlParams = new URLSearchParams(window.location.search);
-        if(urlParams.has('name'))
+        console.log(urlParams);
+        debugger;
+        if(urlParams.has('name')){
             this.init(urlParams.get('name'));        
+            this.debug = urlParams.get('name');
+        }
         else
             this.init();
     },

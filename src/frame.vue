67ef7112e20861ced0f1a3775fdd53197227229f
hansonyu <hansonyu@bccbbs.com>
2021-01-04 14:36:28 +0800

    shgre

--- src/frame.vue
+++ src/frame.vue
@@ -4,7 +4,7 @@
             <div class="welcome-header">數位名片<br>{{line_username}} ，您好</div>
             <div class="welcome-body">
             <div v-if="hasCard" class="h100"><button class="h100" @click="sendCard(myCard)">發送自己名片</button></div><br>
-            <div>{{debug}}<button @click="close">x</button></div>
+            <!-- <div>{{debug}}<button @click="close">x</button></div> -->
             </div>
         </div>
         <div>發送同事名片:</div>
@@ -70,11 +70,16 @@ module.exports = {
                     liffId: "1655456623-oxjPwXjM"
                 })
                 .then(() => {
+                    if (!liff.isLoggedIn()) {
+                        liff.login({ redirectUri: `https://opersei8.github.io/digital-card/?name=${val}` });
+                        // Verify that the domain name and path (https://example.com/path) match the endpoint URL.
+                    }
+
                     this.debug="success"
                     this.getCard()
                     .then(()=>{
                         this.debug=val;
-                        this.sendCard(this.cards.find(emt=>emt.name == val).data,true)
+                        this.sendCard(this.cards.find(emt=>emt.name == val).data)
                         .then(res=>{
                             window.close();
                             liff.closeWindow();
